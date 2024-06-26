ef84ce22ae06420641f965c9550e47cc9b5d7be7
Robby Sarvis <rjs.app.dev@proton.me>
2024-06-25 19:35:53 -0500

    add target folder to gitignore

--- target/debug/build/serde-051a03c4e3ffc675/build_script_build-051a03c4e3ffc675.d
+++
@@ -1,5 +0,0 @@
-/Users/rjs/dev/apialert-sdks/apialerts-rust/target/debug/build/serde-051a03c4e3ffc675/build_script_build-051a03c4e3ffc675: /Users/rjs/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.203/build.rs
-
-/Users/rjs/dev/apialert-sdks/apialerts-rust/target/debug/build/serde-051a03c4e3ffc675/build_script_build-051a03c4e3ffc675.d: /Users/rjs/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.203/build.rs
-
-/Users/rjs/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.203/build.rs:
